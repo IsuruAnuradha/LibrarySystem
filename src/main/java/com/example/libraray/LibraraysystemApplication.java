d01da717d35eb1c5e9331df9316f151735e32b37
Isuru Anuradha <Isuruanuradha99@gmail.com>
2018-08-26 02:55:44 +0800

    reat spring boot project

---
+++ src/main/java/com/example/libraray/LibraraysystemApplication.java
@@ -0,0 +1,12 @@
+package com.example.libraray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraraysystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraraysystemApplication.class, args);
+	}
+}
